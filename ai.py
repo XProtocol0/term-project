7ef106a20c8815658f45195d5f622bd69d802de8
XProtocol0 <tyagiaviral@gmail.com>
2020-04-30 23:38:39 +0530

    Bug fix

--- ai.py
+++ ai.py
@@ -6,13 +6,17 @@ class AI:
 
     @staticmethod
     def move(game):
-        row = random.randint(0, 2)
-        col = random.randint(0, 2)
-        if game.valid_move(row, col):
-            return row, col
-             
-        else:
-            AI.move(game)
+        c = 1
+        while c == 1:
+            row = random.randint(0, 2)
+            col = random.randint(0, 2)
+            if game.valid_move(row, col) == 1:
+                c = game.valid_move(row, col)
+                return row, col
+            
+        
+        
+    
 
 
 
